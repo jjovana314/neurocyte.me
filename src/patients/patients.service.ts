48f2208358e55f7c32b6b7e0d896d6be7c03a13c
Jovana Jovanovic <jjovana314@gmail.com>
2026-03-27 18:34:11 +0100

    added comments for patients service

--- src/patients/patients.service.ts
+++ src/patients/patients.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class PatientsService {}
+export class PatientsService {
+  // store patients history
+  // additional notes regarding the disorder
+  // store information about medication
+  // export patients data in csv
+  // read patients information
+  // update patients information and history
+  // store information about patients symptoms
+}
