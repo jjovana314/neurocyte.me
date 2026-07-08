38828b8e69bac0732f170d8ce202dc6d3d867bc2
Jovana Jovanovic <jjovana314@gmail.com>
2026-07-08 20:26:29 +0200

    updated exception message

--- api/src/common/exceptions/access-to-patient-forbidden.exception.ts
+++ api/src/common/exceptions/access-to-patient-forbidden.exception.ts
@@ -1,7 +1,9 @@
 import { ForbiddenException } from '@nestjs/common';
 
 export class AccessToPatientForbiddenException extends ForbiddenException {
-  constructor(message = 'You can only access data from patients who you added') {
+  constructor(
+    message = 'You can only access data from patients who you added',
+  ) {
     super(message);
   }
 }
