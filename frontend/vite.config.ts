6c4b99d74120ab077d6e83a646754cfc801620bc
Jovana Jovanovic <jjovana314@gmail.com>
2026-07-30 10:04:01 +0200

    vite config updated

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
+    sourcemapIgnoreList: false,
     proxy: {
-      '/auth': 'http://localhost:3000',
-      '/patients': 'http://localhost:3000',
+      "/auth": "http://localhost:3000",
+      "/patients": "http://localhost:3000",
     },
   },
-})
+  build: {
+    sourcemap: true,
+  },
+});
